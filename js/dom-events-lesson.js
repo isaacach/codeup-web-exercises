bf4cf9060e8b95a3ef5265ed961593f0371c8d92
isaacachenbach <isaac.achenbach1@gmail.com>
2023-03-14 12:12:50 -0500

    finished dom exercises

---
+++ js/dom-events-lesson.js
@@ -0,0 +1,21 @@
+let pageWrapper = document.querySelector('.page-wrapper');
+let toggleMenu = document.querySelector('.burger-wrapper');
+let menuBg = document.querySelector('.mobile-menu-overlay');
+
+toggleMenu.addEventListener('click', function(){
+    pageWrapper.classList.toggle('mobile-menu-open');
+})
+
+menuBg.addEventListener('click', function(){
+    pageWrapper.classList.toggle('mobile-menu-open')
+})
+
+
+let dropdowns = document.querySelectorAll('[data-dropdown="parent"]');
+
+dropdowns.forEach(dropdown => {
+    let toggle = dropdown.querySelector('[data-dropdown="toggle"]')
+    toggle.addEventListener('click', function() {
+        dropdown.classList.toggle('open')
+    })
+})
